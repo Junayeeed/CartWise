789c38c52c6fed00b117da056fb1ca7649e4cba5
Junayeeed <junayedxs@gmail.com>
2024-07-02 02:13:24 +0600

    Vercel Deploy Compatible

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,13 @@
 module.exports = ({ env }) => ({
-    "vercel-deploy": {
-      enabled: true,
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_W3zlS9Bz8to8Say4fgK0wrFTuaYm/CiBF2OFdNr",
+      apiToken: "o9RbjMy93cCsNXBVuFVPPeNf",
+      appFilter: "cart-wise",
+      teamFilter: "Personal",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
-  });
+  },
+});
